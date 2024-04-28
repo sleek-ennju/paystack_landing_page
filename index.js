daad525c4e5f174b01bf7c1d53678e32fd1204b0
sleek-ennju <ihenachochukwudi@gmail.com>
2024-04-28 22:09:06 +0100

    completed navigation animations for mobile screens

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 let hamburger = document.getElementById("hamburger");
 let navGroup = document.getElementById("navGroup");	
-let firstLink = document.getElementById("firstLink");
-let subContainer = document.getElementById("sub-1");
-let backIcon = document.getElementById("backIcon");
+let navLinks = document.querySelectorAll("[data-navlink]");
+let subContainer1 = document.getElementById("sub-1");
+let subContainer2 = document.getElementById("sub-2");
+let subContainer3 = document.getElementById("sub-3");
+let subContainer4 = document.getElementById("sub-4");
+let subBackIcons = document.querySelectorAll("[data-subicon]");
+
+
 
 // mobile nav view functionality
-hamburger.addEventListener("click", function(){
+hamburger.addEventListener("click", ()=>{
     
     if(navGroup.style.visibility == "visible"){
         navGroup.style.visibility = "hidden";
@@ -17,12 +22,39 @@ hamburger.addEventListener("click", function(){
     }
 });
 
-// /subcontainer functionality
-firstLink.addEventListener("click", ()=>{
-    navGroup.style.left = "-100%";
-    subContainer.style.left = "0";
+navLinks.forEach(link => {
+    // /subcontainer functionality
+    link.addEventListener("click", ()=>{
+        //main nav container should move to the left
+        navGroup.style.left = "-100%"; 
+        
+        // while the sub containers takes it's position
+        if(link.dataset.navlink == "1"){
+            subContainer1.style.left = "0";
+        }else if(link.dataset.navlink == "2"){
+            subContainer2.style.left = "0";
+        }else if(link.dataset.navlink == "3"){
+            subContainer3.style.left = "0";
+        }else if(link.dataset.navlink == "4"){
+            subContainer4.style.left = "0";
+        }
+    });
 });
-backIcon.addEventListener("click", ()=>{
-    subContainer.style.left = "100%";
-    navGroup.style.left = "0";
+
+subBackIcons.forEach( backIcon => {
+    backIcon.addEventListener("click", ()=>{
+        if(backIcon.dataset.subicon == "sub-1"){
+            subContainer1.style.left = "100%";
+        }else if(backIcon.dataset.subicon == "sub-2"){
+            subContainer2.style.left = "100%";
+        }else if(backIcon.dataset.subicon == "sub-3"){
+            subContainer3.style.left = "100%";
+        }else if(backIcon.dataset.subicon == "sub-4"){
+            subContainer4.style.left = "100%";
+        }
+        navGroup.style.left = "0";
+    });
 });
+
+
+
